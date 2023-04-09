ac0f431664a6fe9c0b79cb5556fca70ba2fcf3f0
Leonid Zotov <zotovleonid@icloud.com>
2023-04-09 15:52:53 +0300

    fix: add cors

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,10 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
-  app.enableCors();
+  app.enableCors({
+    origin: 'http://127.0.0.1:3030',
+    credentials: true,
+  });
   const configService = app.get(ConfigService);
   const port = configService.get('PORT');
   await app.listen(port);
