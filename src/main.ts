aa46d7c2b9b6acb4023505290b34bcce79842549
Leonid Zotov <zotovleonid@icloud.com>
2023-04-14 15:41:44 +0300

    feat: swagger init

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import * as cookieParser from 'cookie-parser';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -13,6 +14,15 @@ async function bootstrap() {
   });
   const configService = app.get(ConfigService);
   const port = configService.get('PORT');
+
+  const config = new DocumentBuilder()
+    .setTitle('Shiba CRM api')
+    .setDescription('Api for SHiba CRM')
+    .setVersion('1.0')
+    .addTag('API')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(port);
 }
 bootstrap();
